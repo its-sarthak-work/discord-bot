336f2beedee44cd29f2f0e9e5c5302be841c29ea
its-sarthak-work <2510350022@geu.ac.in>
2026-05-13 09:29:25 +0530

    added express server

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is alive!");
+});
+
+app.listen(3000, () => {
+  console.log("Web server running");
+});
 const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
